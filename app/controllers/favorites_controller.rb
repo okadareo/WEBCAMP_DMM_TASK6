f8a618fe93003d53da4748800ad39f75300b7f15
okadareo <okadareo0614@gmail.com>
2021-01-12 06:55:22 +0000

    Finish Task6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,14 @@
 class FavoritesController < ApplicationController
 
   def create
-    @books = Book.all
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    @books = Book.all
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    @books = Book.all
   end
 end
